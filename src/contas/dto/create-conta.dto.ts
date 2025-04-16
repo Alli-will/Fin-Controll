33932be82dfd3bcd0ff730aff73809e7073b2306
Alli-will <126529852+Alli-will@users.noreply.github.com>
2025-04-15 21:01:45 -0300

    Ajuste validator

--- src/contas/dto/create-conta.dto.ts
+++ src/contas/dto/create-conta.dto.ts
@@ -1,10 +1,20 @@
-import { IsString } from "class-validator";
+import { IsDate, IsNumber, IsPositive, IsString } from "class-validator";
 
 export class CreateContaDto {
+    @IsString()
     descricao: String;
+    
+    @IsString()
     categoria: String;
+    
+    @IsString()
     formapgto: String;
+    
+    @IsDate()
     data: Date;
+    
+    @IsNumber()
+    @IsPositive()
     valor: number;
 }
 
