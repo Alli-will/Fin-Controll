f23783d79616094baee7f0f22c1f21f6aad38003
Alli-will <126529852+Alli-will@users.noreply.github.com>
2025-06-11 21:15:10 -0300

    alterações de uso

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 5000);
+
+  app.enableCors();
+
+  const port = process.env.PORT || 5000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
